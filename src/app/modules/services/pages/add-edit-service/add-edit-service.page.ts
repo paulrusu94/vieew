30fdee4560ff1d84b8857485a118dee38cfea024
Bogdan Pop <bogdan.pop.fe@amplicare.com>
2023-11-25 16:18:22 +0200

    changes

--- src/app/modules/services/pages/add-edit-service/add-edit-service.page.ts
+++ src/app/modules/services/pages/add-edit-service/add-edit-service.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Auth } from 'aws-amplify';
+import { Subject } from 'rxjs';
+import { APIService } from 'src/app/API.service';
+
 
 @Component({
   selector: '[appAddEditServicesPage]',
@@ -8,12 +11,28 @@ import { Auth } from 'aws-amplify';
   styleUrls: ['./add-edit-service.page.scss'],
 })
 export class AddEditServicesPage implements OnInit, OnDestroy {
+  public _unsubscribe: Subject<boolean> = new Subject<boolean>();
+  
+  public form: FormGroup;
 
   constructor(
+    private apiService: APIService,
+    private formBuilder: FormBuilder,
     private router: Router,
-  ) {}
+  ) {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      description: ['', Validators.compose([Validators.required])],
+      price: ['', Validators.compose([Validators.required])],
+    });
+  }
 
   ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.apiService);
+    this.apiService.CreateService(this.form.value)
+  }
   
   ngOnDestroy() {}
 }
