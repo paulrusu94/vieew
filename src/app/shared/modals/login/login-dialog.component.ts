73c5d95a726a4721e2962e5aa93ff775be4cd116
Ionut gabriel Codeus <codeusionut@Ionuts-MacBook-Pro.local>
2024-07-26 18:50:43 +0300

    LoginRegisterDialog

--- src/app/shared/modals/login/login-dialog.component.ts
+++ src/app/shared/modals/login/login-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators, ValidationMessagesBuilder } from 'src/app/shared/forms';
 import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { RegisterDialogComponent } from '../register/register-dialog.component';
+// Services
+import { ModalService } from 'src/app/shared/services/modal.service';
 
 @Component({
   selector: '[appLoginDialog]',
@@ -15,7 +18,8 @@ export class LoginDialogComponent implements OnInit, OnDestroy {
 
   constructor(
     private formBuilder: FormBuilder,
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal,
+    private modalService: ModalService
   ) {
     this.form = this.formBuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.email()])],
@@ -31,6 +35,14 @@ export class LoginDialogComponent implements OnInit, OnDestroy {
 
   onSubmit() {
     console.log('SUBMIT');
+  }
+
+  public openRegister(): void {
+    const dialog = this.modalService.open(RegisterDialogComponent, { size: 'md' });
+    dialog.result.then(
+      () => { },
+      () => { },
+    );
   }
 
   ngOnDestroy() {}
