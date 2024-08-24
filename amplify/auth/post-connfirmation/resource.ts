e882a121fcf059d6a413c73a57943f347e1249d2
Paul Rusu <paulrusu@192.168.1.64>
2024-08-25 00:25:53 +0300

    Fix deploy.

--- amplify/auth/post-connfirmation/resource.ts
+++ amplify/auth/post-connfirmation/resource.ts
@@ -2,4 +2,7 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const postConfirmation = defineFunction({
   name: 'post-confirmation',
+  environment: {
+    USER_TABLE: process.env.USER_TABLE!
+  }
 });
