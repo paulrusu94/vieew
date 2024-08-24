783577de1c451676b0cb3c2be269eaaf28eefce7
Paul Rusu <paulrusu@192.168.1.64>
2024-08-24 23:58:43 +0300

    Fix deploy.

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,10 +3,6 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
     name: 'vieewBucket',
     access: (allow) => ({
-        'media/*': [
-            allow.guest.to(['read']),
-            allow.authenticated.to(['read', 'write', 'delete'])
-        ],
         'media/content/*': [
             allow.guest.to(['read']),
             allow.authenticated.to(['read', 'write', 'delete'])
