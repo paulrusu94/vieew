e15fd84bb36b0b283bc69e4d639d59f5471510e0
Bogdan Pop <bogdan.pop.fe@amplicare.com>
2024-11-18 15:38:20 +0200

    upgrade modal

--- src/app/shared/modals/upgrade-account/upgrade-account-dialog.component.ts
+++ src/app/shared/modals/upgrade-account/upgrade-account-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, ValidationMessagesBuilder } from 'src/app/shared/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,20 +10,55 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class UpgradeAccountDialogComponent implements OnInit, OnDestroy {
   
-  public selectedOption: number | null = null;
+  public step: string = 'select';
+  public form: FormGroup;
+  public errorMessages: any;
+  public serverError: string = '';
 
   constructor(
     public activeModal: NgbActiveModal,
-  ) { }
+    private formBuilder: FormBuilder,
+  ) {
+    this.form = this.formBuilder.group({
+      industry: ['', Validators.compose([Validators.required])],
+    });
+  }
 
   ngOnInit() {}
 
-  selectOption(option: number) {
-    this.selectedOption = option;
+  private forceValidation() {
+    const controls = this.form.controls;
+
+    Object.keys(controls).map(key => controls[key].markAllAsTouched());
+
+    this.errorMessages = ValidationMessagesBuilder.buildValidationMessages(this.form);
+  }
+
+  async onSubmit() {
+    this.serverError = '';
+    this.forceValidation();
+    if(this.form.valid) {
+      // SEND THE DATA TO SERVER IN ORDER TO UPGRAGE THE ACCOUNT
+      console.log(this.form);
+      this.step = 'success';
+    }
+  }
+
+  selectOption(step: string, selectedOption: string) {
+    this.step = step;
+    if(this.step === 'form' && selectedOption !== 'vieewer') {
+      this.form.addControl('name', this.formBuilder.control(null, Validators.compose([Validators.required])));
+      this.form.addControl('username', this.formBuilder.control(null, Validators.compose([Validators.required])));
+      this.form.addControl('picture', this.formBuilder.control(null, Validators.compose([Validators.required])));
+    } else {
+      this.form.removeControl('name');
+      this.form.removeControl('username');
+      this.form.removeControl('picture');
+    }
   }
 
   goBack() {
-    this.selectedOption = null;
+    this.step = 'select';
   }
   
   ngOnDestroy() {}
